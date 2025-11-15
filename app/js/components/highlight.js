5dafd318a8415bc5db5d59028df5d0a561c1be84
KingOffDarkness <braguta.denis2007@gmail.com>
2025-11-15 21:18:26 +0200

    hljs fix

--- app/js/components/highlight.js
+++ app/js/components/highlight.js
@@ -1,23 +1,15 @@
-// import hljs from 'highlight.js';
-// import 'highlight.js/styles/a11y-dark.min.css'
-// import CopyButtonPlugin from 'highlightjs-copy'
-// import 'highlightjs-copy/dist/highlightjs-copy.min.css'
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github-dark.min.css'
+import CopyButtonPlugin from 'highlightjs-copy'
+import 'highlightjs-copy/dist/highlightjs-copy.min.css'
 
-// hljs.addPlugin(new CopyButtonPlugin({
-// 	autohide: false,
-// }))
+window.hljs = hljs
+
+import('highlightjs-line-numbers.js/dist/highlightjs-line-numbers.min.js').then(() => {
+  hljs.addPlugin(new CopyButtonPlugin({ autohide: false }))
+  hljs.highlightAll()
+  hljs.initLineNumbersOnLoad()
+});
 
 
-// hljs.highlightAll()
 
-import Prism from 'prismjs';
-
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.min.css';
-import 'prismjs/themes/prism-tomorrow.min.css';
-import 'prismjs/plugins/toolbar/prism-toolbar.min.js';
-import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
-import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js';
-
-Prism.highlightAll()
